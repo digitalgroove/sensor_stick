b9c6f3e1f7b0b1fb047765b337dc69c5f187cfb1
Roberto Zegers <rzegers@puc.cl>
2018-09-07 08:24:01 -0700

    Add filtering steps to callback function for Point Cloud Subscriber: *Downsample your point cloud by applying a Voxel Grid Filter *Apply a Pass Through Filter to isolate the table and objects *Perform RANSAC plane fitting to identify the table *Use ExtractIndices Filter to create two new P-C for table and objects

--- scripts/segmentation.py
+++ scripts/segmentation.py
@@ -3,31 +3,94 @@
 # Import modules
 from pcl_helper import *
 
-# TODO: Define functions as required
+# Define functions as required
 
 # Callback function for your Point Cloud Subscriber
 def pcl_callback(pcl_msg):
 
-    # TODO: Convert ROS msg to PCL data
-
-    # TODO: Voxel Grid Downsampling
-
-    # TODO: PassThrough Filter
+    # Convert ROS msg to PCL data
+    # Takes in a ROS message of type PointCloud2 and converts it to PCL PointXYZRGB format
+    pcl_data = ros_to_pcl(pcl_msg)
 
 
-    # TODO: RANSAC Plane Segmentation
+    ## Voxel Grid filter
+    # A voxel grid filter allows to downsample the data
 
-    # TODO: Extract inliers and outliers
+    # Create a VoxelGrid filter object for our input point cloud
+    vox = pcl_data.make_voxel_grid_filter()
+
+    # Choose a voxel (also known as leaf) size
+    # Note: using 1 is a poor choice of leaf size
+    # Units of the voxel size (or leaf size) are in meters
+    # Experiment and find the appropriate size!
+    LEAF_SIZE = 0.01   
+
+    # Set the voxel (or leaf) size  
+    vox.set_leaf_size(LEAF_SIZE, LEAF_SIZE, LEAF_SIZE)
+
+    # Call the filter function to obtain the resultant downsampled point cloud
+    cloud_filtered = vox.filter()
+
+
+    ## PassThrough filter
+    # It allows to crop a part by specifying an axis and cut-off values
+
+    # Create a PassThrough filter object.
+    passthrough = cloud_filtered.make_passthrough_filter()
+
+    # Assign axis and range to the passthrough filter object.
+    # Here axis_min and max is the height with respect to the ground
+    filter_axis = 'z'
+    passthrough.set_filter_field_name(filter_axis)
+    axis_min = 0.6 # to retain only the tabletop and the objects sitting on the table
+    axis_max = 1.1
+    passthrough.set_filter_limits(axis_min, axis_max)
+
+    # Finally use the filter function to obtain the resultant point cloud. 
+    cloud_filtered = passthrough.filter()
+    
+
+    ## RANSAC plane segmentation
+    # Identifys points that belong to a particular model (plane, cylinder, box, etc.)
+
+    # Create the segmentation object
+    seg = cloud_filtered.make_segmenter()
+
+    # Set the model you wish to fit 
+    seg.set_model_type(pcl.SACMODEL_PLANE)
+    seg.set_method_type(pcl.SAC_RANSAC)
+
+    # Max distance for a point to be considered fitting the model
+    # Experiment with different values for max_distance 
+    # for segmenting the table
+    max_distance = 0.01
+    seg.set_distance_threshold(max_distance)
+
+    # Call the segment function to obtain set of inlier indices and model coefficients
+    inliers, coefficients = seg.segment()
+
+
+    ## Extract inliers
+    # Allows to extract points from a point cloud by providing a list of indices
+    cloud_table = cloud_filtered.extract(inliers, negative=False)
+
+
+    ## Extract outliers
+    # With the negative flag True we retrieve the points that do not fit the RANSAC model
+    cloud_objects = cloud_filtered.extract(inliers, negative=True)
+    #filename = 'extracted_outliers.pcd'
 
     # TODO: Euclidean Clustering
 
     # TODO: Create Cluster-Mask Point Cloud to visualize each cluster separately
 
-    # TODO: Convert PCL data to ROS messages
+    # Convert PCL data to ROS messages
+    ros_cloud_objects = pcl_to_ros(cloud_objects)
+    ros_cloud_table = pcl_to_ros(cloud_table)
 
     # Publish ROS messages
-      pcl_objects_pub.publish(pcl_msg)
-      pcl_table_pub.publish(pcl_msg)
+    pcl_objects_pub.publish(ros_cloud_objects)
+    pcl_table_pub.publish(ros_cloud_table)
 
 
 if __name__ == '__main__':
